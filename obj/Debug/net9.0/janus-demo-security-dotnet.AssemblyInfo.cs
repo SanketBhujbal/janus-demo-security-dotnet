5583d6a506a5c4b4e7b4fa28017429f30f36db7c
SecurityBrain <securitybrain@aci.local>
2026-09-03 21:01:15 +0530

    security: autonomous fixes for security/autofix-20260903-153114

--- obj/Debug/net9.0/janus-demo-security-dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/janus-demo-security-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("janus-demo-security-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a688f7ce1adb24fcc912d6c29c1214653f23d19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a1de4bdf6e254a1c829798e83b6adb3624dcb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("janus-demo-security-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("janus-demo-security-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
